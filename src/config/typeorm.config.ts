c344cd589703b2c45cf025e63a720265b10eab4d
abdullah5361k <abdullah5361k@gmail.com>
2025-07-26 00:11:45 +0500

    fix

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,7 +6,7 @@ export const typeormConfig: TypeOrmModuleOptions = {
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432'),
   username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
+  password: process.env.DB_PASSWORD || 'newpassword',
   database: process.env.DB_DATABASE || 'nest-auth',
   entities: [User],
   synchronize: true,
